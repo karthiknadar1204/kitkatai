f07804ec4906ccb387bf6701a21470511fc6d3f0
karthik <karthiknadar1204@gmail.com>
2024-11-10 15:14:24 +0530

    update tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "FlowBrain",
-  description: "The intelligent note-taking app",
+    title: "StickyNotesAi",
+    description: "Smart notes, seamless organization.",
 };
 
 export default function RootLayout({
