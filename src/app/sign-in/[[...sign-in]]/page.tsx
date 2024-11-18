655926fce065470fe3f3e7fbcd1b3ae0740c7e94
karthik <karthiknadar1204@gmail.com>
2024-11-18 22:30:38 +0530

    metadata

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "FlowBrain - Sign In",
+  title: "StickyNotesAi",
 };
 
 export default function SignInPage() {
